3aa7ca77643c8f6c6c577dd7bff13e345d149707
Michael Yanagisawa <michael.yanagisawa@gmail.com>
2026-05-15 20:58:22 -0400

    fix: remove TermExtractor from pdf_parser __init__.py since the file was deleted

--- market_comps/pdf_parser/__init__.py
+++ market_comps/pdf_parser/__init__.py
@@ -1,5 +1,4 @@
 # market_comps/pdf_parser/__init__.py
-from market_comps.pdf_parser.term_extractor import TermExtractor
 from market_comps.pdf_parser.models import ParserResult, ExtractedTerm
 
-__all__ = ["TermExtractor", "ParserResult", "ExtractedTerm"]
+__all__ = ["ParserResult", "ExtractedTerm"]
